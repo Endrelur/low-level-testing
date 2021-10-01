1bff0e51da7e14b807f1322a65528b1b00a9dd62
Endré Hadzalic <endre.had@gmail.com>
2021-10-01 15:58:41 +0200

    Started task 3

--- start_a.sh
+++ start_a.sh
@@ -1,5 +1,7 @@
 nasm -f elf64 ./2/hello.s
 ld ./2/hello.o
-./a.out
-echo "Exit code:"$?
+#run assembly and redirect stderr output to out.txt
+./a.out 2>out.txt
+#write exitcode to out.txt
+echo "Exit code:"$? >>out.txt
 rm ./2/hello.o a.out
